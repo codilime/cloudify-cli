18d9fc7f03afe7c5ac90714e4fe3144a0534e576
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-07-22 14:05:59 +0200

    CFY-3040 added manager unique id to provider context

--- cloudify_cli/bootstrap/tasks.py
+++ cloudify_cli/bootstrap/tasks.py
@@ -16,6 +16,7 @@
 
 import os
 import urllib
+import uuid
 import json
 import pkgutil
 import tarfile
@@ -715,6 +716,7 @@ def _upload_provider_context(remote_agents_private_key_path,
     cloudify_configuration = ctx.node.properties['cloudify']
     cloudify_configuration['cloudify_agent']['agent_key_path'] = \
         remote_agents_private_key_path
+    cloudify_configuration['manager_uid'] = str(uuid.uuid4())
     provider_context['cloudify'] = cloudify_configuration
     ctx.instance.runtime_properties[PROVIDER_RUNTIME_PROPERTY] = \
         provider_context
